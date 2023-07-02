b64cc62e5ef532f661aa6ec170d7e43f98b4f76e
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-07-02 03:02:10 -0600

    currently created the users routes but currently getting password error in the insomnia api endpoint app

--- RecipeApp/server/routes/users-routes.js
+++ RecipeApp/server/routes/users-routes.js
@@ -1,11 +1,54 @@
+require("dotenv").config({
+  path: "/Users/mohamadelgendy/Desktop/MERN projects/RecipeApp/server/jwt-secret/.env",
+});
+
 const express = require("express");
 const usersRoutes = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Users = require("../models/users-models");
 
-usersRoutes.get("/register/", (req, res) => {
-  const { username, password } = req.body;
-  const userExists = Users.find({ username });
-  if (userExists) con;
+//create a new user given username and password
+usersRoutes.post("/register/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userExists = await Users.findOne({ username });
+    if (userExists) {
+      return res.status(409).json({ errorMessage: "User already exists!" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await Users.create({ username, hashedPassword });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+});
+
+usersRoutes.post("/login/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userExists = await Users.findOne({ username });
+    if (!userExists) {
+      return res.status(401).json({ errorMessage: "User not found!" });
+    }
+    const user = userExists;
+    bcrypt.compare(password, user.password, (err, verifiedUser) => {
+      if (err) {
+        return res.status(403).json({ errorMessage: err });
+      }
+      const accessToken = jwt.sign(
+        { username: verifiedUser.username, password: verifiedUser.password },
+        process.env.ACCESS_TOKEN_SECRET
+      );
+      res.status(201).json({ accessToken: accessToken });
+    });
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+});
+
+usersRoutes.post("/logout/", async (req, res) => {
+  res.status(204).json({ msg: "Successfully logged out!" });
 });
 
 module.exports = usersRoutes;
--- RecipeApp/server/server.js
+++ RecipeApp/server/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const usersRoutes = require("./routes/users-routes");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+require("dotenv").config({
+  path: "./mongoosedb/.env",
+});
+
+mongoose.connect(process.env.MONGODB_CONN_STRING).then(() => {
+  console.log("DATABASE IS UP AND RUNNING!");
+});
 
 app.use("/users", usersRoutes);
 
-app.listen(5000, () => {
+app.listen(4000, () => {
   console.log("SERVER IS UP AND RUNNING");
 });
