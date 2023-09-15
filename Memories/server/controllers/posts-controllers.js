db5789c623501792b4a902f98ac74d6d25fd3cab
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-09-14 19:35:17 -0600

    creating controllers for post routes such as getting posts and creating a post

--- Memories/server/controllers/posts-controllers.js
+++ Memories/server/controllers/posts-controllers.js
@@ -1,4 +1,25 @@
-// function controller for getting posts
-const getPosts = (req, res) => {};
+import Post from "../models/post-model.js";
 
-export { getPosts };
+// function controller for getting posts
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find(); // array of all posts
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ errorMessage: error.message });
+  }
+};
+
+// function controller for creating a post
+const createPost = async (req, res) => {
+  const postBody = req.body;
+  const createdPost = new Post(postBody);
+  try {
+    await createdPost.save();
+    res.status(201).json(createdPost);
+  } catch (error) {
+    res.status(409).json({ errorMessage: error.message });
+  }
+};
+
+export { getPosts, createPost };
--- Memories/server/routes/posts-routes.js
+++ Memories/server/routes/posts-routes.js
@@ -2,9 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-import { getPosts } from "../controllers/posts-controllers.js";
+import { getPosts, createPost } from "../controllers/posts-controllers.js";
 
 // route for getting posts
 router.get("/", getPosts);
+
+// route for creating a post
+router.post("/", createPost);
 
 export default router;
